d620296eb9063f0a5cbcb008eed5fcc51a712937
KateKharchenko <kharchenko.k@gmail.com>
2025-06-27 16:16:25 +0300

    Redirect from / to trending

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<script setup>
+// Import required composables
+import { onMounted } from 'vue';
+import { useRouter } from 'vue-router';
+
+// Redirect from / to /trending
+const router = useRouter();
+
+// Redirect on component mount
+onMounted(() => {
+  router.push('/trending');
+});
+</script>
+
+<template>
+  <div>
+    <!-- This page will redirect to /trending -->
+    <p>Redirecting to trending page...</p>
+  </div>
+</template>
